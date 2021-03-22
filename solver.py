08f66b422324d2c884b69d86d998a81a1695d122
Aydar <me@aydar.media>
2021-03-22 21:58:29 +0300

    Update solver.py

--- solver.py
+++ solver.py
@@ -4,7 +4,7 @@ COPYRIGHT DOTERUROD & LESTERRRY,
 2021
 '''''''''''''''''''''''''''''
 
-cookie = "gc_visit_255773=%7B%22id%22%3A2524776305%2C%22sid%22%3A1454671991%7D; gc_visitor_255773=%7B%22id%22%3A1706929101%2C%22sfix%22%3A1%7D; gc_counter_255773=%7B%22id%22%3A1454671991%2C%22partner_code_id%22%3Anull%2C%22ad_offer_id%22%3Anull%2C%22last_activity%22%3A%222021-03-22+21%3A31%3A54%22%2C%22user_id%22%3A156030741%2C%22utm_id%22%3Anull%2C%22fuid%22%3Anull%2C%22fpid%22%3Anull%2C%22city_id%22%3Anull%7D; PHPSESSID5=b4ce07a91b99b58db2ec240cb46a5d64; __cfduid=d09487b73782c7f7d8f9552e79e769f311616437853"
+cookie = "Замените этот текст на свои куки"
 
 links = [
 "https://online.patriotsport.moscow/pl/teach/control/lesson/view?id=194854525&editMode=0",
